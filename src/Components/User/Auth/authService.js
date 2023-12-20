ca12c0a43338c63c299bbacb537f8962e5f950db
Vignesh100799 <baluvignesh422@gmail.com>
2023-12-20 22:05:41 +0530

    First commit

---
+++ src/Components/User/Auth/authService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { config } from "../../../config/config";
+
+
+export const login = async (value) => {
+  try {
+    const response = await axios.post(`${config.userApi}/login`,value);
+   
+    if (response.status===200) {
+     localStorage.setItem("token",response.data.token)
+      return true;
+    } else {
+      throw new Error("Invalid username or password");
+    }
+  } catch (error) {
+    console.error("Login failed:", error.message);
+    throw new Error(error.message);
+  }
+};
+
+
+export const isAuthenticated = () => {
+  return !!localStorage.getItem("token");
+};
